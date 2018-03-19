2f319a6796fc47b0e37b4fa5d9ad7241540f8af0
sheedych <37516762+sheedych@users.noreply.github.com>
2018-03-19 08:26:57 -0700

    Added Phong model

--- Assignment 3/raytracer/light_source.cpp
+++ Assignment 3/raytracer/light_source.cpp
@@ -17,6 +17,34 @@ void PointLight::shade(Ray3D& ray) {
 	// It is assumed at this point that the intersection information in ray 
 	// is available.  So be sure that traverseScene() is called on the ray 
 	// before this function.  
+	
+	//Chris's contribution begins
+	Vector3D normal = ray.intersection.normal;
+	Point3D point = ray.intersection.point;
+	Vector3D viewVec = -1.0 * ray.dir;
 
+	Color ambient = ray.intersection.mat.ambient;
+	Color diffuse = ray.intersection.mat.diffuse;
+	Color specular = ray.intersection.mat.specular;
+	double specular_exp = ray.intersection.mat.specular_exp;
+
+	Point3D lightPos = get_position();
+
+	//Ambient 
+	Color color = ambient;
+
+	//Diffuse
+	Vector3D lightVec = lightPos - point
+	lightVec.normalize()
+	color = color + max(0.0, normal.dot(viewVec));
+
+	//Specular
+	Vector3D incVec = -1.0 * lightVec;
+	Vector3D refVec = 2.0 * normal.dot(lightVec)*normal - lightVec;
+	double cosTheta = max(0.0, viewVec.dot(refVec));
+	color = color + specular * pow(cosTheta, specular_exp);
+
+	ray.col = color;
+	//Chris's contribution ends
 }
 
