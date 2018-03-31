7d4430396175254fb827fb8f030e2a5b2747614b
sheedych <37516762+sheedych@users.noreply.github.com>
2018-03-31 16:21:17 -0700

    updated main

--- Assignment 3/Part B/raytracer/main.cpp
+++ Assignment 3/Part B/raytracer/main.cpp
@@ -60,7 +60,7 @@ int main(int argc, char* argv[])
 	scene.push_back(sphere);
 
 	// add unit planes to make into room
-	SceneNode* back = new SceneNode(new UnitSquare(), &mirror);
+	SceneNode* back = new SceneNode(new UnitSquare(), &grey);
 	scene.push_back(back);
 	SceneNode* front = new SceneNode(new UnitSquare(), &grey);
 	scene.push_back(front);
@@ -72,6 +72,20 @@ int main(int argc, char* argv[])
 	scene.push_back(top);
 	SceneNode* bottom = new SceneNode(new UnitSquare(), &wood);
 	scene.push_back(bottom);
+
+	//Add unit planes to make mirror
+	SceneNode* backMirror = new SceneNode(new UnitSquare(), &mirror);
+	scene.push_back(backMirror);
+
+	//Add unit planes to make frame
+	SceneNode* leftFrontM = new SceneNode(new UnitSquare(), &wood);
+	scene.push_back(leftFrontM);
+	SceneNode* rightFrontM = new SceneNode(new UnitSquare(), &wood);
+	scene.push_back(rightFrontM);
+	SceneNode* topFrontM = new SceneNode(new UnitSquare(), &wood);
+	scene.push_back(topFrontM);
+	SceneNode* bottomFrontM = new SceneNode(new UnitSquare(), &wood);
+	scene.push_back(bottomFrontM);
 
 	//add unit planes to make light source box
 	SceneNode* lightBack = new SceneNode(new UnitSquare(), &obsidian);
@@ -91,9 +105,6 @@ int main(int argc, char* argv[])
 	scene.push_back(inLeft);
 	SceneNode* inRight = new SceneNode(new UnitSquare(), &obsidian);
 	scene.push_back(inRight);
-
-
-
 
 	// Apply some transformations to the sphereS
 	double factor1[3] = { 1.0, 1.0, 1.0 };
@@ -124,6 +135,35 @@ int main(int argc, char* argv[])
 	top->translate(Vector3D(10,20,10));
 	top->rotate('x', 90);
 	top->scale(Point3D(0,0,0), factor2);
+
+	// Apply transformations to make the mirror
+	double factorM1[3] = {10,10,10};
+  	backMirror->translate(Vector3D(0.25, 10, 10));
+	backMirror->rotate('y', 90);
+	backMirror->scale(Point3D(0, 0, 0), factorM1);
+
+	// Apply transformations to make the frame
+	double factorM2[3] = {0.5, 10.5, 1};
+	leftFrontM->translate(Vector3D(0.5,10,15));
+	leftFrontM->rotate('y', -90);
+	leftFrontM->scale(Point3D(0,0,0), factorM2);
+	
+	rightFrontM->translate(Vector3D(0.5,10,5));
+	rightFrontM->rotate('y', -90);
+	rightFrontM->scale(Point3D(0,0,0), factorM2);
+	
+	topFrontM->translate(Vector3D(0.5, 15, 10));
+	topFrontM->rotate('y', -90);
+	topFrontM->rotate('z', 90);
+	topFrontM->scale(Point3D(0,0,0), factorM2);
+	
+	bottomFrontM->translate(Vector3D(0.5, 5, 10));
+	bottomFrontM->rotate('y', -90);
+	bottomFrontM->rotate('z', 90);
+	bottomFrontM->scale(Point3D(0,0,0), factorM2);
+	
+	
+	//bottomSideM
 
 	// Apply transformations to make the light box
 	double factor3[3] = {2, 2, 1 };
