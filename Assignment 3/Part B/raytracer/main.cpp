b76c88a9f9c79b515ac24bb91bb1a5dd90f5576c
sheedych <37516762+sheedych@users.noreply.github.com>
2018-03-30 21:48:01 -0700

    Added recursive ray tracing, built new environment
    added recursive ray tracing to raytracer.cpp
    added reflectIndex to mat class in util.cpp
    created a new environment to render in main.cpp
    
    There are some problems with recursive ray tracing it seems. For some reason there is a bit of noise in the reflection vector, you'll see when you compile it. Also the right wall doesn't seem to be the right colour, but only for one of the cameras. for the other camera it's fine. Anyway we really gotta get our shit together on this one cause it's due wednesday
    
    pce

---
+++ Assignment 3/Part B/raytracer/main.cpp
@@ -0,0 +1,120 @@
+/***********************************************************
+	
+	Starter code for Assignment 3
+
+***********************************************************/
+
+#include <cstdlib>
+#include "raytracer.h"
+
+int main(int argc, char* argv[])
+{
+	// Build your scene and setup your camera here, by calling 
+	// functions from Raytracer.  The code here sets up an example
+	// scene and renders it from two different view points, DO NOT
+	// change this if you're just implementing part one of the 
+	// assignment.  
+	Raytracer raytracer;
+	LightList light_list;
+	Scene scene;   
+
+	int width = 320;
+	int height = 240;
+
+	if (argc == 3) {
+		width = atoi(argv[1]);
+		height = atoi(argv[2]);
+	}
+	
+	// Define materials for shading.
+	Material red(Color(0.39 , 0.0, 0.0), Color(0.784,0.0,0.0),
+		Color(0.7, 0.0, 0.0),
+		51.2);
+	Material grey(Color(0, 0, 0), Color(0.784,0.784,0.784),
+		Color(0.7,0.7,0.7),
+		12.8);
+	Material white(Color(0,0,0), Color(0.9,0.9,0.9), 
+		Color(0.99,0.99,0.99), 
+		12.8, 0.5);
+
+	// Defines a point light source.
+	PointLight* pLight = new PointLight(Point3D(10,10,50), Color(0.9,0.9,0.9));
+	light_list.push_back(pLight);
+	
+	//Chris's Contribution begins
+
+	// Add a unit sphere into the scene with material mat.
+	SceneNode* sphere = new SceneNode(new UnitSphere(), &red);
+	scene.push_back(sphere);
+
+	// add unit planes to make into room
+	SceneNode* back = new SceneNode(new UnitSquare(), &grey);
+	scene.push_back(back);
+	SceneNode* front = new SceneNode(new UnitSquare(), &grey);
+	scene.push_back(front);
+	SceneNode* left = new SceneNode(new UnitSquare(), &grey);
+	scene.push_back(left);
+	SceneNode* right = new SceneNode(new UnitSquare(), &grey);
+	scene.push_back(right);
+	SceneNode* top = new SceneNode(new UnitSquare(), &grey);
+	scene.push_back(top);
+	SceneNode* bottom = new SceneNode(new UnitSquare(), &white);
+	scene.push_back(bottom);
+
+	// Apply some transformations to the sphere
+	double factor1[3] = { 1.0, 1.0, 1.0 };
+	sphere->translate(Vector3D(5, 5, 5));
+	sphere->scale(Point3D(0, 0, 0), factor1);
+
+	// Apply transformations to make the room
+	double factor2[3] = {20, 20, 20 };
+	back->translate(Vector3D(0, 10, 10));
+	back->rotate('y', 90);
+	back->scale(Point3D(0, 0, 0), factor2);
+	
+	front ->translate(Vector3D(20,10,10));
+	front->rotate('y', -90);
+	front->scale(Point3D(0,0,0), factor2);
+
+	left->translate(Vector3D(10, 10, 20));
+	left->scale(Point3D(0, 0, 0), factor2);
+	left->rotate('y', 180);
+
+	right->translate(Vector3D(10, 10, 0));
+	right->scale(Point3D(0, 0, 0), factor2);
+
+	bottom->translate(Vector3D(10,0,10));
+	bottom->rotate('x', -90);
+	bottom->scale(Point3D(0,0,0), factor2);
+
+	top->translate(Vector3D(10,20,10));
+	top->rotate('x', 90);
+	top->scale(Point3D(0,0,0), factor2);
+
+	//Chris's contribution ends
+
+
+	// Render the scene, feel free to make the image smaller for
+	// testing purposes.	
+	Camera camera1(Point3D(18, 10, 5), Vector3D(-3, -1, 1), Vector3D(0, 1, 0), 60.0);
+	Image image1(width, height);
+	raytracer.render(camera1, scene, light_list, image1); //render 3D scene to image
+	image1.flushPixelBuffer("view1.bmp"); //save rendered image to file
+
+	// Render it from a different point of view.
+	Camera camera2(Point3D(10, 5, 10), Vector3D(-1, 0, -1), Vector3D(0, 1, 0), 60.0);
+	Image image2(width, height);
+	raytracer.render(camera2, scene, light_list, image2);
+	image2.flushPixelBuffer("view2.bmp");
+
+	// Free memory
+	for (size_t i = 0; i < scene.size(); ++i) {
+		delete scene[i];
+	}
+
+	for (size_t i = 0; i < light_list.size(); ++i) {
+		delete light_list[i];
+	}
+
+	return 0;
+}
---
+++ Assignment 3/Part B/raytracer/raytracer.cpp
@@ -0,0 +1,127 @@
+/***********************************************************
+	
+	Starter code for Assignment 3
+
+	Implementations of functions in raytracer.h, 
+	and the main function which specifies the scene to be rendered.	
+
+***********************************************************/
+
+
+#include "raytracer.h"
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+
+void Raytracer::traverseScene(Scene& scene, Ray3D& ray)  {
+	for (size_t i = 0; i < scene.size(); ++i) {
+		SceneNode* node = scene[i];
+
+		if (node->obj->intersect(ray, node->worldToModel, node->modelToWorld)) {
+			ray.intersection.mat = node->mat;
+		}
+	}
+}
+
+void Raytracer::computeTransforms(Scene& scene) {
+	// right now this method might seem redundant. But if you decide to implement 
+	// scene graph this is where you would propagate transformations to child nodes
+		
+	for (size_t i = 0; i < scene.size(); ++i) {
+		SceneNode* node = scene[i];
+
+		node->modelToWorld = node->trans;
+		node->worldToModel = node->invtrans; 
+	}
+}
+
+void Raytracer::computeShading(Ray3D& ray, LightList& light_list) {
+	for (size_t  i = 0; i < light_list.size(); ++i) {
+		LightSource* light = light_list[i];
+		
+		// Each lightSource provides its own shading function.
+		// Implement shadows here if needed.
+		light->shade(ray);        
+	}
+}
+
+Color Raytracer::shadeRay(Ray3D& ray, Scene& scene, LightList& light_list, int depth = 0) {
+	Color col(0.0, 0.0, 0.0); 
+	traverseScene(scene, ray); 
+
+	// Don't bother shading if the ray didn't hit 
+	// anything.
+	if (!ray.intersection.none) {
+		computeShading(ray, light_list); 
+		col = ray.col;  
+		
+	}
+
+	// You'll want to call shadeRay recursively (with a different ray, 
+	// of course) here to implement reflection/refraction effects.  
+
+	//Chris's contribution begins
+	//reflection
+	int maxDepth = 1;
+	float reflectIndex = ray.intersection.mat->reflectIndex;
+
+	if (!ray.intersection.none){
+		if(reflectIndex != 0 && depth < maxDepth){
+			//get reflection vector
+			Point3D point = ray.intersection.point;
+			Vector3D normal = ray.intersection.normal;
+			Vector3D incidentVec = ray.dir;
+			Vector3D reflectVec = incidentVec - 2 * incidentVec.dot(normal) * normal;
+			reflectVec.normalize();
+			Ray3D reflectRay = Ray3D(point, reflectVec);
+
+			//get reflection color
+			Color refCol = shadeRay(reflectRay, scene, light_list, depth + 1);
+
+			//return final color
+			Color finalCol = reflectIndex*refCol + (1-reflectIndex)*col;
+			//finalCol.clamp();
+			return finalCol;
+		}
+	}
+	//Chris's contribution ends
+
+
+	return col; 
+}	
+
+void Raytracer::render(Camera& camera, Scene& scene, LightList& light_list, Image& image) {
+	computeTransforms(scene);
+
+	Matrix4x4 viewToWorld;
+	double factor = (double(image.height)/2)/tan(camera.fov*M_PI/360.0);
+
+	viewToWorld = camera.initInvViewMatrix();
+
+	// Construct a ray for each pixel.
+	for (int i = 0; i < image.height; i++) {
+		for (int j = 0; j < image.width; j++) {
+			// Sets up ray origin and direction in view space, 
+			// image plane is at z = -1.
+			Point3D origin(0, 0, 0);
+			Point3D imagePlane;
+			imagePlane[0] = (-double(image.width)/2 + 0.5 + j)/factor;
+			imagePlane[1] = (-double(image.height)/2 + 0.5 + i)/factor;
+			imagePlane[2] = -1;
+
+			
+			
+			Ray3D ray;
+			// TODO: Convert ray to world space  
+			//define ray using origin, direction vector.
+			// Logan's contribution begins
+			ray.origin = viewToWorld * origin;
+			ray.dir = viewToWorld * (imagePlane - origin);
+			ray.dir.normalize();
+			// Logan's contribution ends
+			Color col = shadeRay(ray, scene, light_list); 
+			image.setColorAtPixel(i, j, col);			
+		}
+	}
+}
+
